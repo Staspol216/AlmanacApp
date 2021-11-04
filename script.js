0d1d331f684186adc6b46aeb6ca2853fd77dd3d9
Stas Polishchuk <staspol216@gmail.com>
2021-11-04 18:33:58 +0300

    feat: add starting popup functional

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const rightBtn = document.querySelector(".calendar__button-right");
 const daysCalendar = document.querySelector(".calendar__days");
 
 const date = new Date();
+const month = date.getMonth();
+const year = date.getFullYear();
 
 const monthsArr = [
     "Январь",
@@ -33,14 +35,28 @@ const weekdaysArr = [
 
 let clicked = null;
 let events = localStorage.getItem("events") ? JSON.parse(localStorage.getItem("events")) : [];
+const newEvent = document.querySelector(".popup");
 
+
+function openPopup(date) {
+    clicked = null;
+
+    const eventForDay = events.find(e => e.date === clicked);
+
+    if (eventForDay) {
+        console.log ("already exists");
+    } else {
+        newEvent.style.display = "block";
+    }
+
+}
 
 function displayCalendar() {
     const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-
     const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay() - 1;
     const daysNextMonths = 7 - lastDayIndex - 1;
+
     let firstDayIndex = date.getDay() - 1;
 
     date.setDate(1);
@@ -57,20 +73,25 @@ function displayCalendar() {
         const daySquare = document.createElement("div");
         daySquare.classList.add("day");
 
-        const dayString = "";
+        let dayString = "";
 
         if (i < (firstDayIndex + 1)) {
             daySquare.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${prevLastDay - countPrevDays + 1}</p>`;
+            dayString = `${prevLastDay - countPrevDays + 1}/${month-1}/${year}`;
             countPrevDays--;
             indexWeekdays++;
         } else if (i <= lastDay && indexWeekdays < 7) {
             daySquare.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${i - firstDayIndex}</p>`;
+            dayString = `${i - firstDayIndex}/${month}/${year}`;
             indexWeekdays++;
         } else if (i <= lastDay + firstDayIndex) {
             daySquare.innerHTML = `<p>${i - firstDayIndex}</p>`;
+            dayString = `${i - firstDayIndex}/${month}/${year}`;
         } else { 
             daySquare.innerHTML = `<p>${i - firstDayIndex - lastDay}</p>`;
+            dayString = `${i - firstDayIndex - lastDay}/${month+1}/${year}`;
         }
+        daySquare.addEventListener('click', () => openPopup(dayString));
         daysCalendar.appendChild(daySquare);
     }
 }
