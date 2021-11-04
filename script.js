b83661a651c3aaa2849df0538fb7f0cdd5fc7c76
Stas Polishchuk <staspol216@gmail.com>
2021-11-04 09:06:03 +0300

    feat: creating dynamic cell with JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let monthAndYear = document.querySelector(".calendar__months-year");
+const monthAndYear = document.querySelector(".calendar__months-year");
+const calendarDays = document.querySelector(".calendar__days");
 
 const date = new Date();
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+console.log(lastDay);
 
 const monthsArr = [
     "Январь",
@@ -20,4 +23,12 @@ const monthsArr = [
 monthAndYear.innerHTML = monthsArr[date.getMonth()] + ` ${date.getFullYear()}`;
 
 let days = "";
+
+
+for (let i = 1; i <= lastDay; i++) {
+    days += `<div><p>${i}</p></div>`;
+}
+calendarDays.innerHTML = days;
+
+
 
