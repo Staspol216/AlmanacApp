5313ebf074cea416ac65ac9b1de70d2a77296945
Stas Polishchuk <staspol216@gmail.com>
2021-11-04 11:01:13 +0300

    feat: add dynamic previus and next days of months, add name of weekdays in  head cells

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const monthAndYear = document.querySelector(".calendar__months-year");
 const calendarDays = document.querySelector(".calendar__days");
 
 const date = new Date();
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-console.log(lastDay);
+
+let firstDayIndex = date.getDay() - 1;
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay() - 1;
+
+date.setDate(1);
 
 const monthsArr = [
     "Январь",
@@ -17,17 +22,45 @@ const monthsArr = [
     "Сентябрь",
     "Октябрь",
     "Ноябрь",
-    "Декабрь",
+    "Декабрь"
+];
+
+const weekdaysArr = [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье"
 ];
 
 monthAndYear.innerHTML = monthsArr[date.getMonth()] + ` ${date.getFullYear()}`;
 
 let days = "";
+let indexWeekadys = 0;
 
+while (firstDayIndex > 0) {
+    days += `<div><p>${weekdaysArr[indexWeekadys]}, ${prevLastDay - firstDayIndex + 1}</p></div>`;
+    firstDayIndex--;
+    indexWeekadys++;
+}
 
 for (let i = 1; i <= lastDay; i++) {
-    days += `<div><p>${i}</p></div>`;
+    if ( indexWeekadys < 7) {
+    days += `<div><p>${weekdaysArr[indexWeekadys]}, ${i}</p></div>`;
+    indexWeekadys++;
+    } else {
+        days += `<div><p>${i}</p></div>`;
+    }
 }
+
+const daysNextMonths = 7 - lastDayIndex - 1;
+
+for (let j = 1; j <= daysNextMonths; j++) {
+    days += `<div><p>${j}</p></div>`;
+}
+
 calendarDays.innerHTML = days;
 
 
