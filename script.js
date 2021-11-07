61ca853f2c34ac87008afecc52b5bf6a11626685
Stas Polishchuk <staspol216@gmail.com>
2021-11-07 23:15:17 +0300

    feat: add quickEvent popup without functional

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const leftBtn = document.querySelector(".calendar__button-left");
 const rightBtn = document.querySelector(".calendar__button-right");
 const daysCalendar = document.querySelector(".calendar__days");
 
-
 const newEventPopup = document.querySelector(".popup");
 const popupInput = document.querySelectorAll(".popup__input");
 const popupInputDescr = document.querySelector(".popup__input-descr");
@@ -11,6 +10,7 @@ const closeBtn = document.querySelector(".popup__close-btn");
 const readyBtn = document.getElementById("readyBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 const todayBtn = document.querySelector(".calendar__today-button");
+const addBtn = document.querySelector(".header__add-button");
 
 
 const existEventPopup = document.querySelector(".popup_existEvent");
@@ -21,6 +21,9 @@ const popupDescrEvent = document.getElementById("descrExistEvent");
 const closeBtnExistEvent = document.getElementById("closeBtnExistEvent");
 const readyBtnExistEvent = document.getElementById("readyBtnExistEvent");
 const deleteBtnExistEvent = document.getElementById("deleteBtnExistEvent");
+
+const quickEventPopup = document.querySelector(".popup_quickEvent");
+const closeBtnQuickEvent = document.getElementById("closeBtnQuickEvent");
 
 let scrollCount = 0;
 let clicked = null;
@@ -70,6 +73,7 @@ function openPopup(date) {
 function closePopup() {
     newEventPopup.style.display = "none";
     existEventPopup.style.display = "none";
+    quickEventPopup.style.display = "none";
     popupInput.forEach(function(text) {
         text.value = "";
     });
@@ -105,10 +109,12 @@ function editDescr() {
 }
 
 function displayCalendar() {
+
     const date = new Date();
     if (scrollCount !== 0) {
         date.setMonth(new Date().getMonth() + scrollCount);
     }
+
     date.setDate(1);
     const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
@@ -125,24 +131,24 @@ function displayCalendar() {
     daysCalendar.innerHTML = "";
 
     for (let i = 1; i <= sumAllDays; i++ ) {
-        const daySquare = document.createElement("div");
-        daySquare.classList.add("day");
+        const dayCell = document.createElement("div");
+        dayCell.classList.add("day");
 
         let dayString = "";
         if (i < (firstDayIndex + 1)) {
-            daySquare.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${prevLastDay - countPrevDays + 1}</p>`;
+            dayCell.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${prevLastDay - countPrevDays + 1}</p>`;
             dayString = `${prevLastDay - countPrevDays + 1}/${date.getMonth()-1}/${date.getFullYear()}`;
             countPrevDays--;
             indexWeekdays++;
         } else if (i <= lastDay && indexWeekdays < 7) {
-            daySquare.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${i - firstDayIndex}</p>`;
+            dayCell.innerHTML = `<p>${weekdaysArr[indexWeekdays]}, ${i - firstDayIndex}</p>`;
             dayString = `${i - firstDayIndex}/${date.getMonth()}/${date.getFullYear()}`;
             indexWeekdays++;
         } else if (i <= lastDay + firstDayIndex) {
-            daySquare.innerHTML = `<p>${i - firstDayIndex}</p>`;
+            dayCell.innerHTML = `<p>${i - firstDayIndex}</p>`;
             dayString = `${i - firstDayIndex}/${date.getMonth()}/${date.getFullYear()}`;
         } else { 
-            daySquare.innerHTML = `<p>${i - firstDayIndex - lastDay}</p>`;
+            dayCell.innerHTML = `<p>${i - firstDayIndex - lastDay}</p>`;
             dayString = `${i - firstDayIndex - lastDay}/${date.getMonth()+1}/${date.getFullYear()}`;
         }
 
@@ -150,29 +156,29 @@ function displayCalendar() {
         const eventP = document.createElement("p");
         const nameP = document.createElement("p");
         if (eventForDay) {
-            daySquare.style.backgroundColor = "#C2E4FE";
+            dayCell.style.backgroundColor = "#C2E4FE";
             eventP.classList.add("day__event");
             eventP.textContent = eventForDay.Event;
             nameP.classList.add("day__name");
             nameP.textContent = eventForDay.Name;
-            daySquare.appendChild(eventP);
-            daySquare.appendChild(nameP);
+            dayCell.appendChild(eventP);
+            dayCell.appendChild(nameP);
         } 
 
-        daysCalendar.appendChild(daySquare);
+        daysCalendar.appendChild(dayCell);
 
-        daySquare.addEventListener("mouseenter", function(event) {
+        dayCell.addEventListener("mouseenter", function(event) {
             if (eventP.textContent || nameP.textContent) {
                 event.target.style.backgroundColor = "#27A1FF";
             }
         });
 
-        daySquare.addEventListener("mouseleave", function(event) {
+        dayCell.addEventListener("mouseleave", function(event) {
             if (eventP.textContent || nameP.textContent) {
                 event.target.style.backgroundColor = "#C2E4FE";
             }
         });
-        daySquare.addEventListener('click', () => openPopup(dayString));
+        dayCell.addEventListener('click', () => openPopup(dayString));
     }
 }
 
@@ -191,6 +197,9 @@ todayBtn.addEventListener("click", function() {
     scrollCount = 0;
     displayCalendar();
 });
+addBtn.addEventListener("click", function() {
+    quickEventPopup.style.display = "block";
+});
 
 closeBtn.addEventListener("click", closePopup);
 readyBtn.addEventListener("click", saveEvent);
@@ -199,5 +208,6 @@ deleteBtn.addEventListener("click", deleteEvent);
 deleteBtnExistEvent.addEventListener("click", deleteEvent);
 closeBtnExistEvent.addEventListener("click", closePopup);
 readyBtnExistEvent.addEventListener("click", editDescr);
+closeBtnQuickEvent.addEventListener("click", closePopup);
 
 displayCalendar();
